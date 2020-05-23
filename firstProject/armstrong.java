f93006cf86915c493b6165ade0ff726feebbf2b1
Shravya Yermal <37536963+ShhravyaYerrmal@users.noreply.github.com>
2020-05-23 14:51:23 +0530

    Update armstrong.java

--- firstProject/armstrong.java
+++ firstProject/armstrong.java
@@ -2,29 +2,30 @@ package demo;
 
 public class armstrong {
 	
-		// Main method to get the armstrong number verifiaction
-		public static void main(String[] args) {
-			 int num = 1635, rem, digit = 0, sum = 0; 
-	          
-	        //Copy the value for num in num  
-	        int i=num;
-	          
-	        // Find total digits in num  
-	        digit = (int) Math.log10(num) + 1; 
-	          
-	        // Calculate sum of power of digits  
-	        while(num > 0) 
-	        {  
-	            rem = num % 10; 
-	            sum = sum + (int)Math.pow(rem, digit); 
-	            num = num / 10; 
-	        } 
-	          
-	        // Check for Armstrong number  
-	        if(i == sum) 
-	            System.out.println(i+" is an Armstrong number");
-	        else
-	            System.out.println(i+" is not an Armstrong number");
+		// Main method to get the armstrong number verification
+		public static void main(String[] args)
+		{
+			int num = 1635, rem, digit = 0, sum = 0; 
+
+			//Copy the value for num in num  
+			int i=num;
+
+			// Find total digits in num  
+			digit = (int) Math.log10(num) + 1; 
+
+			// Calculate sum of power of digits  
+			while(num > 0) 
+			{  
+			    rem = num % 10; 
+			    sum = sum + (int)Math.pow(rem, digit); 
+			    num = num / 10; 
+			} 
+
+			// Check for Armstrong number  
+			if(i == sum) 
+			    System.out.println(i+" is an Armstrong number");
+			else
+			    System.out.println(i+" is not an Armstrong number");
 		}
 	
 	
